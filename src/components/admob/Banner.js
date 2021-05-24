79660c4acf45a2719db869d99c6643f084afb7fc
Lena <tsybulenko.helen@gmail.com>
2021-05-24 16:28:12 +0300

    admob banner

--- src/components/admob/Banner.js
+++ src/components/admob/Banner.js
@@ -3,18 +3,16 @@ import {
     AdMobBanner,
 } from 'expo-ads-admob';
 import {View} from "react-native";
-import {useSelector} from "react-redux";
 
 export const Banner = () => {
-    const pro = useSelector(state => state.settings.proPurchased)
     const bannerError = () => {
         console.log("An error");
         return;
     }
-    return( pro ? null :
+    return(
         <View style={{backgroundColor: 'black', width: '100%', alignItems: 'center'}}>
         <AdMobBanner
-            bannerSize="banner"
+            bannerSize="fullBanner"
             adUnitID="ca-app-pub-3485393765261217/5109333554" //Real ID
             // adUnitID="ca-app-pub-3940256099942544/2934735716" //Test ID
             servePersonalizedAds // true or false
